dd093c70dd175b10c186dd82675f4a4fc02bc48a
Sergio Estevao <sergio.estevao@tigerspike.com>
2013-01-07 23:15:36 +0000

    Added test cases for missing categories methods.

--- UnitTest/Source/UIColor+SEAdditionsTest.m
+++ UnitTest/Source/UIColor+SEAdditionsTest.m
@@ -11,6 +11,18 @@
 
 @implementation UIColor_SEAdditionsTest
 
+- (void) test_colorWithRGB{
+    STAssertEqualObjects([UIColor colorWithRGB:0x000000],[UIColor colorWithRed:0 green:0 blue:0 alpha:1], @"Test against black color");
+    
+    STAssertEqualObjects([UIColor colorWithRGB:0xFFFFFF],[UIColor colorWithRed:1 green:1 blue:1 alpha:1], @"Test against white color");
+}
+
+- (void) test_colorWithRedGreenBlue{
+    STAssertEqualObjects([UIColor colorWithRed:0 green:0 blue:0],[UIColor colorWithRed:0 green:0 blue:0 alpha:1], @"Test against black color");
+    
+    STAssertEqualObjects([UIColor colorWithRed:255 green:255 blue:255],[UIColor colorWithRed:1 green:1 blue:1 alpha:1], @"Test against white color");
+}
+
 - (void) test_htmlHexString{
     STAssertEqualObjects([[UIColor blackColor] htmlHexString],@"#000000", @"Test against black color");
     
